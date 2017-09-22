b76ed4ff6344d306c192986efa097ea85aff13f0
Jan Michael Auer <jan.auer@sentry.io>
2017-09-22 12:54:47 +0200

    feat: Implement basic app workflow

--- index.js
+++ index.js
@@ -1,10 +1,107 @@
-module.exports = (robot) => {
-  // Your code here
-  console.log('Yay, the app was loaded!')
+/**
+ * Installation target type: User
+ */
+const TYPE_USER = 'User';
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+/**
+ * Installation target type: Organization
+ */
+const TYPE_ORGANIZATION = 'Organization';
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+class Reporter {
+  constructor(github, installation) {
+    this.github = github;
+    this.installation = installation;
+
+    this.setupUsers();
+  }
+
+  addUser(user) {
+    if (user.type !== TYPE_USER) {
+      return;
+    }
+
+    this.users[user.id] = {
+      id: user.id,
+      login: user.login,
+    };
+
+    // TODO: Get and update email addresses
+    // TODO: Get and update timezone
+    // TODO: Get notification schedule
+    // TODO: Schedule notifications for this user
+  }
+
+  removeUser(user) {
+    // TODO: Remove scheduled notifications
+    delete this.users[user.id];
+  }
+
+  setupUsers() {
+    this.users = {};
+
+    const account = this.installation.account;
+    const targetType = this.installation.target_type;
+
+    if (targetType === TYPE_ORGANIZATION) {
+      const request = this.github.orgs.getMembers({ org: account.login, per_page: 100 });
+      this.github.paginate(request, result => result.data.forEach(user => this.addUser(user)));
+    } else if (targetType === TYPE_USER) {
+      this.addUser(account);
+    } else {
+      // TODO: Log a warning
+    }
+  }
+
+  getPullRequestsByUser(user) {
+    const query = `is:pr is:open review-requested:${user.login}`;
+    const request = this.github.search.issues({ q: query, per_page: 100 });
+    return this.github.paginate(request, response => response.data);
+    // TODO: Filter PRs by staleness. Check if there is a search query filter for that
+  }
+
+  teardown() {
+    // TODO: Remove schedule for this installation
+  }
 }
+
+const reporters = {};
+
+async function addInstallation(github, installation) {
+  const id = installation.account.id;
+  if (reporters[id] == null) {
+    reporters[id] = new Reporter(github, installation);
+  }
+}
+
+async function removeInstallation(installation) {
+  const id = installation.account.id;
+  const reporter = reporters[id];
+  if (reporter) {
+    reporter.teardown();
+    delete reporters[id];
+  }
+}
+
+async function setupRobot(robot) {
+  const github = await robot.auth();
+
+  github.paginate(github.apps.getInstallations({ per_page: 100 }), (result) => {
+    result.data.forEach(async (installation) => {
+      const installationGithub = await robot.auth(installation.id);
+      addInstallation(installationGithub, installation);
+    });
+  });
+
+  robot.on('installation.created', async (context) => {
+    addInstallation(context.github, context.payload.installation);
+  });
+
+  robot.on('installation.deleted', async (context) => {
+    removeInstallation(context.payload.installation);
+  });
+
+  // TODO(ja): Listen for new PR review requests...
+}
+
+module.exports = setupRobot;
